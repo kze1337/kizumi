d4f4f27f474c053caeb66927db91afc23ae26904
Ajiora <169074018+kze1337@users.noreply.github.com>
2026-05-30 01:12:33 +0900

    extra setup for fixxing encoding error on some systems

---
+++ extra.sh
@@ -0,0 +1,10 @@
+$HOME/kizumi/venv/bin/pip install --upgrade nodriver
+
+iconv -f latin-1 -t utf-8 \
+  $HOME/kizumi/venv/lib/python3.14/site-packages/nodriver/cdp/network.py \
+  -o /tmp/network_fixed.py \
+  && mv /tmp/network_fixed.py \
+  $HOME/kizumi/venv/lib/python3.14/site-packages/nodriver/cdp/network.py
+
+  sed -i '1s/^/# -*- coding: latin-1 -*-\n/' \
+  $HOME/kizumi/venv/lib/python3.14/site-packages/nodriver/cdp/network.py
